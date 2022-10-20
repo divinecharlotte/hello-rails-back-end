44772bb092c628794895dd05016b4a9c2a293543
charlotte divine <divinemaina@gmail.com>
2022-10-19 22:50:16 -0700

    add required changes

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,8 +2,8 @@ module Api
   module V1
     class GreetingsController < ApplicationController
       def index
-        @greeting = Greeting.order('RANDOM()').first
-        render json: @greeting
+        greeting = Greeting.order('RANDOM()').first
+        render json: greeting
       end
     end
   end
